a5c62ec6a629ebe687882dc516ec91de8572e688
Sai Binayak Rout <saibinayak111@gmail.com>
2023-08-28 19:34:44 +0530

    gave it lttl dynamic look

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Student.", "Frontend Devloper.", "Webpage Designer."],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 300,
+    loop: true
+
+})
